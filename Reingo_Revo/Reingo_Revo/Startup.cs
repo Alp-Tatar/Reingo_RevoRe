3dcc5b989cbd85f96c189938e863b5af34189124
Alp Tatar <alpmerttatar@gmail.com>
2022-11-08 12:01:30 -0500

    Construction
    Inital construction of the project

---
+++ Reingo_Revo/Reingo_Revo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Reingo_Revo.Startup))]
+namespace Reingo_Revo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
